4f607c5027faabb5eda94923c15c7b2e7db66880
Crowdbotics <team@crowdbotics.com>
2024-02-06 06:17:23 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "bitccch911casino-46026",
+  url: "https://bitccch911casino-46026.botics.co",
+  api: "https://bitccch911casino-46026.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
